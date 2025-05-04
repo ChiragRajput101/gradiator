d8f939c96c50f79b1e72a5b230794714bb80c1bc
ChiragRajput101 <chiragrajput245@outlook.com>
2025-05-04 20:12:46 +0530

    makefile

---
+++ Makefile
@@ -0,0 +1,3 @@
+run:
+	@python3 node.py
+	@python3 main.py
--- node.py
+++ node.py
@@ -55,18 +55,3 @@ class Node:
     def __repr__(self):
         return f"Node(value={self.value}, grad={self.grad})"
 
-
-h = 0.1 # sanity check: following the direction of grad to see if Loss increases
- 
-w1 = Node(3.0); w1._label = 'w1'
-w2 = Node(4.0); w2._label = 'w2'
-x1 = Node(-2.0); x1._label = 'x1'
-x2 = Node(12.0); x2._label = 'x2'
-
-e = w1*x1; e._label = 'e'
-f = w2*x2; f._label = 'f'
-
-Loss = e+f; Loss._label = 'Loss'
-print(Loss)
-Loss.backward()
-
